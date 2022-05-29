fd91fc4694b7e3084902c3ef22e4ebc6ddf7055d
mauricio.yaque <lilian.quezada@catolica.edu.sv>
2022-05-29 00:41:59 +0000

    se creo el codigo para la interfaz del ingreso de datos de de notas de los alumnos

--- webservicelaboratorio.php
+++ webservicelaboratorio.php
@@ -1,12 +1,41 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php
+include "vendor/autoload.php";
+$server=new nusoap_server;
+$server->configureWSDL('server','urn:server');
+$server->wsdl->schemaTargetNamespace='urn:server';
+$server->wsdl->addComplexType(
+    'Promedio',
+    'complexType',
+    'struct',
+    'all',
+    '',
+    array(
+          'nombre'=>array('name'=>'nombre','type'=>'xsd:string'),
+          'nota1'=>array('name'=>'laboratorio1','type'=>'xsd:float'),
+          'nota2'=>array('name'=>'laboratorio2','type'=>'xsd:float'),
+          'parcial'=>array('name'=>'parcial','type'=>'xsd:float'),
+          'promedio'=>array('name'=>'promedio','type'=>'xsd:float')
+    )
+);
+
+$server->register(
+    'promedio',
+    array('nombre'=>'xsd:string','laboratorio1'=>'xsd:float', 'laboratorio2'=>'xsd:float', 'parcial'=>'xsd:float'),
+    array('return'=>'tns:Promedio'),
+    'urn:server',
+    'urn:server#promedioServer',
+    'rpc',
+    'encoded',
+    'Funcion que solicita 3 notas y devuelve el promedio final'
+);
+
+function promedio($nombre, $laboratorio1, $laboratorio2, $parcial){
+        'nota2'=>$laboratorio2,
+        'parcial'=>$parcial,
+        'promedio'=>$promedio
+    );
+    return $result;
+}
+
+
+$server->service(file_get_contents("php://input"));
